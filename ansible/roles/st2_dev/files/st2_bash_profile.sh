0a579ab208e89f27018ba5d026afa9388a65f34e
Tomaz Muraus <tomaz@tomaz.me>
2021-04-25 00:02:19 +0200

    Use a more robust way of setting Python 3 binary used for StackStorm make targets and virtualenv.
    Overriding python3 system binary may break Python based system tools such as
    apt.

--- ansible/roles/st2_dev/files/st2_bash_profile.sh
+++ ansible/roles/st2_dev/files/st2_bash_profile.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-PYTHON_VERSION=$(python3 --version | sed "s/Python //g" | sed "s/\.//g" | tr -d "\n")
+PYTHON_BINARY=$(cat /home/vagrant/.st2_python_binary)
+export PYTHON_VERSION=$(echo ${PYTHON_BINARY} | sed "s#/usr/bin/##g")
+
+PYTHON_VERSION_STRING=$(${PYTHON_BINARY} --version | sed "s/Python //g" | sed "s/\.//g" | tr -d "\n")
 
 export VIRTUALENV_DIR=virtualenv-py${PYTHON_VERSION}
 export VIRTUALENV_ST2CLIENT_DIR=virtualenv-st2client-py${PYTHON_VERSION}
 export VIRTUALENV_COMPONENTS_DIR=virtualenv-components-py${PYTHON_VERSION}
+
+echo ""
+echo "Using Python 3 binary for Make targets: ${PYTHON_BINARY} (${PYTHON_VERSION})"
+echo "VIRTUALENV_DIR=${VIRTUALENV_DIR}"
+echo "VIRTUALENV_ST2CLIENT_DIR=${VIRTUALENV_ST2CLIENT_DIR}"
+echo "VIRTUALENV_COMPONENTS_DIR=${VIRTUALENV_COMPONENTS_DIR}"
 
 if [ -d "~/st2" ]; then
     cd ~/st2
