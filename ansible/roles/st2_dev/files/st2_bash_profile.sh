9e20870ae67ebbb07abf973f85e067b3b48460df
Tomaz Muraus <tomaz@tomaz.me>
2021-04-25 13:32:58 +0200

    Update script file.

--- ansible/roles/st2_dev/files/st2_bash_profile.sh
+++ ansible/roles/st2_dev/files/st2_bash_profile.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-PYTHON_BINARY=$(cat /home/vagrant/.st2_python_binary)
-export PYTHON_VERSION=$(echo ${PYTHON_BINARY} | sed "s#/usr/bin/##g")
+
+PYTHON_BINARY_FILE_PATH="/home/vagrant/.st2_python_binary"
+
+if [ ! -f "${PYTHON_BINARY_FILE_PATH}" ]; then
+    echo "${PYTHON_BINARY_FILE_PATH} file doesn't exist. Make sure you ran vagrant provision"
+fi
+
+PYTHON_BINARY=$(cat ${PYTHON_BINARY_FILE_PATH})
+export PYTHON_VERSION=$(echo "${PYTHON_BINARY}" | sed "s#/usr/bin/##g")
 
 PYTHON_VERSION_SHORT_STRING=$(${PYTHON_BINARY} --version | sed "s/Python //g" | sed "s/\.//g" | tr -d "\n")
 
@@ -10,9 +17,11 @@ export VIRTUALENV_COMPONENTS_DIR=virtualenv-components-py${PYTHON_VERSION_SHORT_
 
 echo ""
 echo "Using Python 3 binary for Make targets: ${PYTHON_BINARY} (${PYTHON_VERSION})"
+echo ""
 echo "VIRTUALENV_DIR=${VIRTUALENV_DIR}"
 echo "VIRTUALENV_ST2CLIENT_DIR=${VIRTUALENV_ST2CLIENT_DIR}"
 echo "VIRTUALENV_COMPONENTS_DIR=${VIRTUALENV_COMPONENTS_DIR}"
+echo ""
 
 if [ -d "/home/vagrant/st2" ]; then
     cd /home/vagrant/st2
