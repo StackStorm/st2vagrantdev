d4a29aca1a65048be1fb8d14adaf285700ef6d32
Tomaz Muraus <tomaz@tomaz.me>
2021-04-25 14:13:36 +0200

    Fix ci check, update script.

--- ansible/roles/st2_dev/files/st2_bash_profile.sh
+++ ansible/roles/st2_dev/files/st2_bash_profile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PRINT_INFO=${PRINT_INFO:-"1"}
+
 PYTHON_BINARY_FILE_PATH="/home/vagrant/.st2_python_binary"
 
 if [ ! -f "${PYTHON_BINARY_FILE_PATH}" ]; then
@@ -8,7 +10,9 @@ if [ ! -f "${PYTHON_BINARY_FILE_PATH}" ]; then
 fi
 
 PYTHON_BINARY=$(cat ${PYTHON_BINARY_FILE_PATH})
-export PYTHON_VERSION=$(echo "${PYTHON_BINARY}" | sed "s#/usr/bin/##g")
+PYTHON_BINARY_NAME=$(echo "${PYTHON_BINARY}" | sed "s#/usr/bin/##g")
+
+export PYTHON_VERSION=${PYTHON_BINARY_NAME}
 
 PYTHON_VERSION_SHORT_STRING=$(${PYTHON_BINARY} --version | sed "s/Python //g" | sed "s/\.//g" | tr -d "\n")
 
@@ -16,16 +20,18 @@ export VIRTUALENV_DIR=virtualenv-py${PYTHON_VERSION_SHORT_STRING}
 export VIRTUALENV_ST2CLIENT_DIR=virtualenv-st2client-py${PYTHON_VERSION_SHORT_STRING}
 export VIRTUALENV_COMPONENTS_DIR=virtualenv-components-py${PYTHON_VERSION_SHORT_STRING}
 
-echo ""
-echo "Using Python 3 binary for Make targets: ${PYTHON_BINARY} (${PYTHON_VERSION})"
-echo ""
-echo "VIRTUALENV_DIR=${VIRTUALENV_DIR}"
-echo "VIRTUALENV_ST2CLIENT_DIR=${VIRTUALENV_ST2CLIENT_DIR}"
-echo "VIRTUALENV_COMPONENTS_DIR=${VIRTUALENV_COMPONENTS_DIR}"
-echo ""
+if [ "${PRINT_INFO}" = "1" ]; then
+    echo ""
+    echo "Using Python 3 binary for Make targets: ${PYTHON_BINARY} (${PYTHON_VERSION})"
+    echo ""
+    echo "VIRTUALENV_DIR=${VIRTUALENV_DIR}"
+    echo "VIRTUALENV_ST2CLIENT_DIR=${VIRTUALENV_ST2CLIENT_DIR}"
+    echo "VIRTUALENV_COMPONENTS_DIR=${VIRTUALENV_COMPONENTS_DIR}"
+    echo ""
+fi
 
 if [ -d "/home/vagrant/st2" ]; then
-    cd /home/vagrant/st2
+    cd "/home/vagrant/st2"
 
     if [ -f "${VIRTUALENV_DIR}/bin/activate" ]; then
         source "${VIRTUALENV_DIR}/bin/activate"
